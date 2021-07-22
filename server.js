d795b87c4255a952771be32f7d9b763240f7c828
EajayD <edelos1524@hotmail.com>
2021-07-21 19:24:15 -0700

    added 2 get routes to display index and notes htmls, also added port listener

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
+
+//Establish GET routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+ 
+// GET route to get the notes html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})  
+
+
+  
+// PORT listener
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
+  
